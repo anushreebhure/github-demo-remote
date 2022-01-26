5cdf958d1724f9f26cf362587a3e0fe84c80cc2a
anushreebhure <65760882+anushreebhure@users.noreply.github.com>
2022-01-26 16:36:01 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+#add
+def add(x,y):
+    return x+y
+    #subtract
+def subtract(x,y):
+    return x-y
+    #multiply
+def multiply(x,y):
+    return x*y
+    #divide
+def divide(x,y):
+    if y==0:
+        return DIVIDE_BY_0_ERROR
+    else:
+        return x/y
+        
+        #square implementation 
+def square(x):
+    return x*x
